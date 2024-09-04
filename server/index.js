824314cdbfaa627f0aebcc9b866cc2323ee949f3
Abhinandan <93651229+AE-Hertz@users.noreply.github.com>
2024-09-04 18:39:47 +0530

    issue fixed v2

--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ import authRoutes from "./routes/auth.js";
 dotenv.config();
 const app = express();
 app.use(express.json());
-//app.use(helmet());
-//app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-//app.use(cors());
+app.use(cors());
 
 // Gemini config
 export const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
